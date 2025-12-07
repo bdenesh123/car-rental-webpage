31981d1fa4cc303cdf75007d1c040d2566f0877d
bdenesh123 <drajan@wvb.com>
2025-12-07 16:09:23 +0800

    styling fixes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -75,7 +75,7 @@ const CarCard = ({ car }: CarCardProps) => {
         <div className="car-card__btn-container">
           <CustomButton
             title="View More"
-            containerStyles="w-full py-[16px] rounded-full bg-primary-blue"
+            containerStyles="w-full py-[16px] rounded-full bg-green-600 hover:bg-green-700 transition"
             textStyles="text-white text-[14px] leading-[17px] font-bold"
             rightIcon="/right-arrow.svg"
             handleClick={() => setIsOpen(true)}
