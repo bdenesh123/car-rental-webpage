985cf52475c6a3c982dd3d12262323855c2bf41a
bdenesh123 <drajan@wvb.com>
2025-12-07 18:40:33 +0800

    fixes

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -49,9 +49,10 @@ const CarCard = ({ car }: CarCardProps) => {
         />
       </div>
 
-      <div className="relative flex w-full mt-2">
-        <div className="flex group-hover:invisible w-full justify-between text-grey">
-          <div className="flex flex-col justify-center items-center gap-2">
+      <div className="relative w-full mt-2">
+        {/* Icons */}
+        <div className="flex justify-between text-gray-500 group-hover:invisible">
+          <div className="flex flex-col items-center gap-1">
             <Image
               src="/steering-wheel.svg"
               width={20}
@@ -60,19 +61,29 @@ const CarCard = ({ car }: CarCardProps) => {
             />
             <p className="text-[14px] leading-[17px]">{transmission}</p>
           </div>
-
-          <div className="car-card__icon">
+          <div className="flex flex-col items-center gap-1">
             <Image src="/tire.svg" width={20} height={20} alt="drive" />
-            <p className="car-card__icon-text">{drive.toUpperCase()}</p>
+            <p className="text-[14px] leading-[17px]">{drive.toUpperCase()}</p>
           </div>
-
-          <div className="car-card__icon">
+          <div className="flex flex-col items-center gap-1">
             <Image src="/gas.svg" width={20} height={20} alt="fuel" />
-            <p className="car-card__icon-text">{fuelEconomy}</p>
+            <p className="text-[14px] leading-[17px]">{fuelEconomy}</p>
           </div>
         </div>
 
-        <div className="car-card__btn-container">
+        {/* Desktop View: Button overlays icons on hover */}
+        <div className="absolute top-0 left-0 w-full h-full flex items-end justify-center opacity-0 group-hover:opacity-100 transition-all duration-300 pointer-events-none sm:pointer-events-auto">
+          <CustomButton
+            title="View More"
+            containerStyles="w-11/12 py-[16px] rounded-full bg-green-600 hover:bg-green-700 transition pointer-events-auto"
+            textStyles="text-white text-[14px] leading-[17px] font-bold"
+            rightIcon="/right-arrow.svg"
+            handleClick={() => setIsOpen(true)}
+          />
+        </div>
+
+        {/* Mobile View: Always visible at the bottom */}
+        <div className="mt-3 sm:hidden w-full">
           <CustomButton
             title="View More"
             containerStyles="w-full py-[16px] rounded-full bg-green-600 hover:bg-green-700 transition"
